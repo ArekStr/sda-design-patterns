d91078a31c6a1aad55e5c45e7c591c4013e93302
Arek Strenk <lechpoznan12@gmail.com>
2018-03-03 10:47:29 +0100

    added builder

---
+++ src/main/java/pl/sda/poznan/builder/Program.java
@@ -0,0 +1,9 @@
+package pl.sda.poznan.builder;
+
+public class Program {
+    public static void main(String[] args) {
+        Reservation reservation = new Reservation();
+        reservation.setCity("poznan");
+        reservation.setHeadcount(199);
+    }
+}
